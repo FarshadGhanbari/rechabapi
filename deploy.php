cde0186b9999a104c82111d18ca57d7fd449ea63
oscargonzalez <oscar.gonzalez.varela@gmail.comm>
2015-12-21 18:21:44 +0100

    deploy script with key fromn config file

--- deploy.php
+++ deploy.php
@@ -1,4 +1,9 @@
 <?php
-	// Used to deply from guithub on a Webhook (push)
-	file_put_contents("../deploy.now", "deploy now!", FILE_APPEND | LOCK_EX);
+	include "include/config.php"	
+
+	if (isset($_GET['key']) AND $_GET['key'] == _DEPLOY_KEY_)
+	{
+		// Used to deply from guithub on a Webhook (push)
+		file_put_contents("../deploy.now", "deploy now!", FILE_APPEND | LOCK_EX);
+	}
 ?>
