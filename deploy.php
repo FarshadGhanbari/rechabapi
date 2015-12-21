cbeb744a6809aca0e685c6f45eacea7ac77a2b93
oscargonzalez <oscar.gonzalez.varela@gmail.comm>
2015-12-21 18:26:39 +0100

    fix

--- deploy.php
+++ deploy.php
@@ -1,7 +1,7 @@
 <?php
 	include "include/config.php"	
 
-	if (isset($_GET['key']) AND $_GET['key'] == _DEPLOY_KEY_)
+	if ($_SERVER['HTTP_X_GITHUB_EVENT'] == 'push') {
 	{
 		// Used to deply from guithub on a Webhook (push)
 		file_put_contents("../deploy.now", "deploy now!", FILE_APPEND | LOCK_EX);
